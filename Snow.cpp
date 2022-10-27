e2bb76c9748333533cb239c414e1cfc242b89e8d
Adam <57573881+AdamCarter11@users.noreply.github.com>
2022-10-26 18:32:06 -0700

    figuring out position

--- Snow.cpp
+++ Snow.cpp
@@ -5,12 +5,14 @@ using namespace pe;
 
 //we need this function to override the pure virtual otherwise it derives it from an abstract class which is not okay
 void Snow::CreateParticle(const int index, const float x, const float y) {
-	
+	Vector2f temp;
+	//shape.setPosition(x);
 }
 
 
 void Snow::CreateParticle(const int num, CircleShape& shape) {
 	Vector2f temp;
+	//shape.setPosition();
 
 	for (short i = 0; i < NUM_OF_PARTICLES; ++i) {
 		// Random velocity
