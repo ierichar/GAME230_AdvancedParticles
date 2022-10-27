cec81d12661429542111c5615a53b63dc66d9de6
Adam <57573881+AdamCarter11@users.noreply.github.com>
2022-10-26 18:20:08 -0700

    fixed curly brackets

--- Snow.cpp
+++ Snow.cpp
@@ -12,11 +12,12 @@ void Snow::CreateParticle(const int index, const float x, const float y) {
 void Snow::CreateParticle(const int num, CircleShape& shape) {
 	Vector2f temp;
 
-	for (short i = 0; i < NUM_OF_PARTICLES; ++i)
+	for (short i = 0; i < NUM_OF_PARTICLES; ++i) {
 		// Random velocity
 		temp.x = Random::Range(HI, LO, RAND_MAX);
 		temp.y = Random::Range(HI, LO, RAND_MAX);
 
 		// Random lifespan
 		float rand_lifespan = Random::Range(10.f, 1.f);
+	}
 }
